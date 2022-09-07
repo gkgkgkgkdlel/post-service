4fb65b79e5fadd28362e0d71d876307356a727e2
gkgkgkgkdlel <gkgkgkgkdlel@naver.com>
2022-09-07 15:31:03 +0900

    Chore: 프로젝트 초기 setting

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "posting.apps.PostingConfig",
 ]
 
 MIDDLEWARE = [
@@ -74,13 +76,6 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 
 # Password validation
