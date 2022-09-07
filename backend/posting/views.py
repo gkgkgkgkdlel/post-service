6515e560ae3348ae974bd6b6bbf2d294485f8648
gkgkgkgkdlel <gkgkgkgkdlel@naver.com>
2022-09-07 17:02:55 +0900

    Feat: 게시글 리스트 Paginator 추가

--- backend/posting/views.py
+++ backend/posting/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import redirect, HttpResponse
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.core.paginator import Paginator, EmptyPage
 from .models import Post
 
 
@@ -14,8 +15,19 @@ from .serializers import PostSerializer, PostUpdateSerializer
 class PostView(APIView):
     def get(self, request):
         post_set = Post.objects.all().order_by("-created_at")
+        page = request.GET.get("page")
+        paginator = Paginator(post_set, 20)
+        page_obj = None
 
-        serializer = PostSerializer(post_set, many=True)
+        try:
+            page_obj = paginator.page(page)
+        except EmptyPage:
+            return Response(
+                {"message": "WRONG_PAGE"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        serializer = PostSerializer(page_obj, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
